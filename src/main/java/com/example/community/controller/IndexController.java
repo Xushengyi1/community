c2568f32307388e0fa19177bb7f5962d6aae2596
Xushengyi <2414657523@qq.com>
2022-04-14 14:51:52 +0800

    init repo

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
-public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
-    }
-
+public class IndexController {
+    @GetMapping("/")
+    public String index() {return "index";}
 }
