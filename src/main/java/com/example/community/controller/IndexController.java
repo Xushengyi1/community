9bd2262b0ffb807e82ead1ee2cfbc041fbe932f2
Xushengyi <2414657523@qq.com>
2022-04-14 14:44:55 +0800

    init repo

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 
 }
